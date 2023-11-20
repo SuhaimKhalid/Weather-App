999da039c2f213f5022296457671e7cc3c6089e5
Suhaim Khalid <suhaimkhalid007@gmail.com>
2023-11-20 20:05:20 +0000

    Create html today section area dynamically

--- Assets/script.js
+++ Assets/script.js
@@ -8,7 +8,31 @@ event.preventDefault();
 var search = $("#search-input").val().trim();
 
 console.log(search);
-
+createhtml(search)
 
 });
 
+function createhtml(search){
+
+// Delcare some variable to create element in Html
+var section = $("<section>");
+var h1 = $("<h1>");
+var i1 = $("<i>");
+var p1 = $("<p>");
+var p2 = $("<p>");
+var p3 = $("<p>");
+
+
+// Add Classes and Id to tags
+section.attr("id","today");
+
+
+// Pick the main Div to show newly created Html
+var Text_area = $(".Text_area");
+Text_area.empty();
+Text_area.append(section);
+section.append(h1,p1,p2,p3);
+h1.append(i1);
+
+h1.text(search);
+}
